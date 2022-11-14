bebececa992a8eaf98e36d8d73657ee8c7d73402
rschoolu <107159650+rschoolu@users.noreply.github.com>
2022-11-13 22:12:08 -0500

    added the cookie clicker formatter option (doesn't format the thousnads)

--- trimmyFormat.js
+++ trimmyFormat.js
@@ -1,4 +1,4 @@
-function trimmyFormat(number,shortExtensions) {
+function trimmyFormat(number,shortExtensions,formatThousands) {
     String.prototype.count=function(c) { 
         var result = 0, i = 0;
         for(i;i<this.length;i++)if(this[i]==c)result++;
@@ -9,8 +9,12 @@ function trimmyFormat(number,shortExtensions) {
     if (shortExtensions == false) {
         extensions = ['', ' thousand', ' million', ' billion', ' trillion', ' quadrillion', ' quintillion', ' sextillion', ' septillion', ' octillion', ' nonillion', ' decillion', ' undecillion',' duodecillion',' tredecillion',' quattuordecillion',' quinquadecillion',' sexdecillion',' septendecillion',' octodecillion',' novendecillion',' vigintillion',' unvigintillion']
     }
+    let requiredExtensions = 1;
+    if (formatThousands == false) {
+    	requiredExtensions = 2;
+    }
     let extensionNumber = formattedNumber.count(',')
-    if ((extensionNumber == 0) || (extensionNumber > extensions.length - 1)) {
+    if ((extensionNumber < requiredExtensions) || (extensionNumber > extensions.length - 1)) {
         return formattedNumber
     }
     let frickShit = (function(){ if(extensionNumber != 1) { return "." + formattedNumber.split(',')[1] } else { return "" } })
